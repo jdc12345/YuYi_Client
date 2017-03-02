c145cfc6265050da778817951eb0ef0ae070a8d3
jdcwy <414266852@qq.com>
2017-03-02 16:21:53 +0800

    试推

--- YuYi_Client/YYLogInVC.m
+++ YuYi_Client/YYLogInVC.m
@@ -9,6 +9,7 @@
 #import "YYLogInVC.h"
 #import "UIColor+colorValues.h"
 #import <Masonry.h>
+#import "UILabel+Addition.h"
 @interface YYLogInVC ()
 
 @end
@@ -52,9 +53,9 @@
     UITextField *telNumberField = [[UITextField alloc]init];
     [self.view addSubview:telNumberField];
     [telNumberField mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(line1.mas_left);
+        make.left.equalTo(telImageView.mas_left).offset(15);
         make.bottom.equalTo(line1.mas_top).offset(-7.5);
-        make.width.height.offset(17);
+        make.width.offset(75);
     }];
 
     //添加line2
@@ -75,10 +76,50 @@
         make.bottom.equalTo(line2.mas_top).offset(-7.5);
         make.width.height.offset(17);
     }];
-    
+    //添加密码textField
+    UITextField *passWordField = [[UITextField alloc]init];
+    [self.view addSubview:passWordField];
+    [passWordField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(passWordImageView.mas_left).offset(15);
+        make.bottom.equalTo(line2.mas_top).offset(-7.5);
+        make.width.offset(75);
+    }];
     //添加获取验证码Btn
+    UIButton *getCodeBtn = [[UIButton alloc]init];
+    [getCodeBtn setTitle:@"获取验证码" forState:UIControlStateNormal];
+    getCodeBtn.titleLabel.font = [UIFont systemFontOfSize:9];
+    [getCodeBtn setBackgroundColor:[UIColor colorWithHexString:@"66cc00"]];
+    [self.view addSubview:getCodeBtn];
+    [getCodeBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(passWordField.mas_right).offset(5);
+        make.bottom.equalTo(line2.mas_top).offset(-7.5);
+        make.width.offset(50);
+        make.height.offset(20);
+    }];
+    //点击事件
+    [getCodeBtn addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchDown];
+    
+    [getCodeBtn addTarget:self action:@selector(buttonClick1:) forControlEvents:UIControlEventTouchUpInside];
     //添加倒计时Label
+    UILabel *countdownLabel = [UILabel labelWithText:@"59 S" andTextColor:[UIColor colorWithHexString:@"f211221"] andFontSize:12];
+    [self.view addSubview:countdownLabel];
+    [countdownLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(getCodeBtn.mas_right).offset(5);
+        make.centerY.equalTo(getCodeBtn);
+        make.width.offset(25);
+        make.height.offset(20);
+    }];
+    [countdownLabel sizeToFit];
     //添加登录Btn
+}
+
+-(void)buttonClick:(UIButton *)button{
+    [button setBackgroundColor:[UIColor colorWithHexString:@"#cccccc"]];
+    
+}
+-(void)buttonClick1:(UIButton *)button{
+//    [button setBackgroundColor:[UIColor colorWithHexString:@"66cc00"]];
+    
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
